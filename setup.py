e56acd19feb333b2775bbcf2c59f0cb572c65d36
Nasir Khalid <Nasirmkhalid24@gmail.com>
2022-08-30 16:29:01 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@ import os
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name='loop_limitation',
     version='0.0.1',
+    author='Tianhao Xie',
+    description='A pytorch cpp extension for evaluating loop limit subdivision surface Resources',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=["loop_limitation"],
+    install_requires=["torch"],
     ext_modules=[
         CppExtension(
             name='loop_limitation',
